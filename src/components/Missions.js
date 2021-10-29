3fb7434d1aef484d656970f799d0f831856dae08
Guilherme <guilhaugusto05@gmail.com>
2021-10-29 15:30:09 -0300

    Requisito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((mission) => {
+          const { name, year, country, destination } = mission;
+          const allProps = {
+            name,
+            year,
+            country,
+            destination,
+          };
+          return <MissionCard key={ name } { ...allProps } />;
+          /* Source: https://stackoverflow.com/questions/51148064/reacts-props-with-the-same-name-as-their-value */
+        })}
       </div>
     );
   }
